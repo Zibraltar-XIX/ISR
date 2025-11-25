ca338746ebfa4609178bb83b944b7a1da75e1dc6
Parzival <johann.parola@vulcore.dev>
2025-11-25 12:35:51 +0100

    v1.1

--- enable-winrm.bat
+++ enable-winrm.bat
@@ -2,3 +2,4 @@ $url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts
 $file = "$env:temp\ConfigureRemotingForAnsible.ps1"
 (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
 powershell.exe -ExecutionPolicy ByPass -File $file
+pause
