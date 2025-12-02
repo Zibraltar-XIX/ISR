73b4dfbd2377ce353f68baab336a95df44463fbe
Parzival <johann.parola@vulcore.dev>
2025-12-02 11:29:39 +0100

    v58

--- test.ps1
+++ test.ps1
@@ -1,1 +1,13 @@
-exit 1
+$disk = Get-WmiObject Win32_LogicalDisk -Filter "DeviceID='C:'"
+
+if ($disk.FreeSpace / 1GB -lt 5) {
+
+ Write-Host "CRITICAL: moins de 5 Go sur C:"
+
+ exit 2
+
+}
+
+Write-Host "OK: espace disque suffisant"
+
+exit 0
